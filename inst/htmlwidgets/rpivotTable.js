0708d753131e429e1f537f713cd68e25f65d0c1f
Jacques Booysen <booysenjacques@gmail.com>
2015-11-21 15:22:11 +0200

    Fixed Shiny Issue in renderUI situations

--- inst/htmlwidgets/rpivotTable.js
+++ inst/htmlwidgets/rpivotTable.js
@@ -6,26 +6,29 @@ HTMLWidgets.widget({
 
     initialize: function(el, width, height) {
 
-	    return {}
+	    return {};
 
     },
 
     renderValue: function(el, x, instance) {
-	    x.data = HTMLWidgets.dataframeToD3(x.data);
+      
+      try {
+  	    x.data = HTMLWidgets.dataframeToD3(x.data);
+	      var derivers = $.pivotUtilities.derivers;
+        var tpl = $.pivotUtilities.aggregatorTemplates;
 
-	    var derivers = $.pivotUtilities.derivers;
-      var tpl = $.pivotUtilities.aggregatorTemplates;
+        x.params.renderers = $.extend(
+          $.pivotUtilities.renderers,
+          $.pivotUtilities.d3_renderers,
+          $.pivotUtilities.c3_renderers
+        );
 
-      x.params.renderers = $.extend(
-        $.pivotUtilities.renderers,
-        $.pivotUtilities.d3_renderers,
-        $.pivotUtilities.c3_renderers
-      );
-
-
-      $('#'+el.id).pivotUI(
-      		x.data, x.params
-      );
+        $('#'+el.id).pivotUI(
+      	  	x.data, x.params
+        );
+      } catch(err) {
+         $('#'+el.id).replaceWith('<div id="pivottable"></div>');  
+      }
 
     },
 
